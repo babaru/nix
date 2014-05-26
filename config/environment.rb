b116d6b07c583501fa7a1765a569308867c8fdcf
yuyang <kristy_yy@126.com>
2014-05-26 13:18:34 +0800

    20140526

--- config/environment.rb
+++ config/environment.rb
@@ -7,12 +7,12 @@ require 'class_ext'
 
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.default_charset = "utf-8"
+#ActionMailer::Base.default_charset = "utf-8"
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
     :address => "smtp.126.com",
     :port => 25,
-    :domain => "126.com",
+    :domain => "www.126.com",
     :authentication => :login,
     :user_name => "kristy_yy@126.com",
     :password => "2311811"
