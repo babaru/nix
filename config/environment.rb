c8be41743b70a7873301f115066d581e75f451c8
yuyang <kristy_yy@126.com>
2014-05-26 12:52:39 +0800

    qqq

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,20 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Nix::Application.initialize!
 require 'class_ext'
+
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.default_charset = "utf-8"
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+    :address => "smtp.126.com",
+    :port => 25,
+    :domain => "126.com",
+    :authentication => :login,
+    :user_name => "kristy_yy@126.com",
+    :password => "2311811"
+}
+
+ExceptionNotification::Notifier.exception_recipients = %w(234016483@qq.com)
+ExceptionNotification::Notifier.email_prefix = "[ERROR] "
+ExceptionNotification::Notifier.sender_address = %("Application Error" <system@iforce.com>)
